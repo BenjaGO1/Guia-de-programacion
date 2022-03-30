02ecdfa4465f609168c2f958c7939d69a3fd0606
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-03-30 11:07:54 -0300

    Ejercicio 8

---
+++ Assets/Scripts/ejercicio8.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio8 : MonoBehaviour
+{
+    public int dia = 0;
+    void Start()
+    {
+        if (dia == 1)
+        {
+            Debug.Log("Hoy es Domingo");
+        }
+        else if (dia == 2)
+        {
+            Debug.Log("Hoy es Lunes");
+        }
+        else if (dia == 3)
+        {
+            Debug.Log("Hoy es Martes");
+        }
+         else if (dia == 4)
+        {
+            Debug.Log("Hoy es Miercoles");
+        }
+        else if (dia == 5)
+        {
+            Debug.Log("Hoy es Jueves");
+        }
+        else if (dia == 6)
+        {
+            Debug.Log("Hoy es Viernes");
+        }
+        else if (dia == 1)
+        {
+            Debug.Log("Hoy es Sabado");
+        }
+        else
+        {
+            Debug.Log("El dia ingresado no es valido");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
